9aa6a37751227d980675df42bb8719d152ed7a60
brandmch <101467867+brandmch@users.noreply.github.com>
2023-02-02 22:33:36 -0500

    basic get title functionality

--- src/App.js
+++ src/App.js
@@ -1,30 +1,94 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useEffect, useState } from "react";
+import { Box, Button, TextField, Typography } from "@mui/material";
 
 function App() {
-  fetch(
-    "https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=15&gsrsearch='New_England'"
-  )
-    .then((res) => res.json())
-    .then((data) => console.log(data));
+  const [textInput, setTextInput] = useState("");
+  const [pageList, setPageList] = useState([]);
+  const [numOfTitlesVisible, setNumOfTitlesVisible] = useState(0);
+
+  const searchTitle = () => {
+    let str = textInput
+      .trim()
+      .split("")
+      .reduce((acc, curr) => {
+        return curr === " " ? `${acc}_` : `${acc}${curr}`;
+      }, "");
+
+    fetch(
+      `https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=100&gsrsearch=${str}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        let tempArr = [];
+        for (let i in data.query.pages) {
+          tempArr.push({
+            pageID: data.query.pages[i].pageid,
+            title: data.query.pages[i].title,
+          });
+        }
+        setPageList(tempArr);
+        setNumOfTitlesVisible(5);
+      });
+    console.log(pageList);
+  };
+
+  const handleClick = (item) => {
+    console.log(item);
+    if (item.title === "SHOW MORE") {
+      setNumOfTitlesVisible(numOfTitlesVisible + 5);
+    }
+  };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box
+      display="flex"
+      flexDirection="column"
+      backgroundColor="#000000"
+      alignItems="center"
+      minHeight={"100vh"}
+    >
+      <TextField
+        variant="filled"
+        label="Search"
+        sx={{ backgroundColor: "darkGreen", marginTop: 10 }}
+        value={textInput}
+        onChange={(e) => setTextInput(e.target.value)}
+      />
+      <Button variant="contained" onClick={searchTitle}>
+        GO
+      </Button>
+      {pageList.length > 0
+        ? [
+            ...pageList.slice(0, numOfTitlesVisible),
+            { pageID: 0, title: "SHOW MORE" },
+          ].map((curr) => {
+            return (
+              <Box
+                onClick={() => handleClick(curr)}
+                key={curr.pageID}
+                sx={{
+                  color: "white",
+                  fontWeight: curr.title === "SHOW MORE" ? "bold" : "normal",
+                  padding: 0.5,
+                  "&:hover": {
+                    backgroundColor: "lightblue",
+                    color: "#000000",
+                    cursor: "pointer",
+                  },
+                  "&:active": {
+                    background: "pink",
+                  },
+                }}
+              >
+                {" "}
+                <Typography>{curr.title}</Typography>
+              </Box>
+            );
+          })
+        : null}
+    </Box>
   );
 }
 

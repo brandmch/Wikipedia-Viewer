f128ea486dbe75e1e3b384234f6172c875db1df3
brandmch <101467867+brandmch@users.noreply.github.com>
2023-02-03 21:10:39 -0500

    swag

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useEffect, useState } from "react";
 import { Box, Button, TextField, Typography } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 function parseForURL(str) {
   return str
@@ -12,11 +13,23 @@ function parseForURL(str) {
     }, "");
 }
 
+const theme = createTheme({
+  typography: {
+    palette: {
+      primary: {
+        main: "#96FFFF",
+        dark: "#0B6A6A",
+        light: "#CEFFFF",
+      },
+    },
+    fontFamily: ["Noto Serif Ahom"],
+  },
+});
+
 function App() {
   const [textInput, setTextInput] = useState("");
   const [pageList, setPageList] = useState([]);
   const [numOfTitlesVisible, setNumOfTitlesVisible] = useState(0);
-  const [currentArticle, setCurrentArticle] = useState();
 
   const searchTitle = () => {
     fetch(
@@ -39,58 +52,79 @@ function App() {
     console.log(pageList);
   };
 
+  const handleClickGO = (title) => {
+    if (title === "SHOW MORE") {
+      setNumOfTitlesVisible(numOfTitlesVisible + 5);
+    } else {
+      window.open(`https://en.wikipedia.org/wiki/${parseForURL(title)}`);
+    }
+  };
+
+  const DisplayResults = () => {
+    return pageList.length > 0
+      ? [
+          ...pageList.slice(0, numOfTitlesVisible),
+          { pageID: 0, title: "SHOW MORE" },
+        ].map((curr) => {
+          return (
+            <Box
+              onClick={() => handleClickGO(curr.title)}
+              key={curr.pageID}
+              backgroundColor="primary"
+              border="1px solid white"
+              borderRadius={5}
+              margin={1}
+              color="white"
+              flexWrap="wrap"
+              width={"10rem"}
+              height="3rem"
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              textAlign="center"
+            >
+              {" "}
+              <Typography>{curr.title}</Typography>
+            </Box>
+          );
+        })
+      : null;
+  };
+
   return (
-    <Box
-      display="flex"
-      flexDirection="column"
-      backgroundColor="#000000"
-      alignItems="center"
-      minHeight={"100vh"}
-    >
-      <TextField
-        variant="filled"
-        label="Search"
-        sx={{ backgroundColor: "darkGreen", marginTop: 10 }}
-        value={textInput}
-        onChange={(e) => setTextInput(e.target.value)}
-      />
-      <Button variant="contained" onClick={searchTitle}>
-        GO
-      </Button>
-      {pageList.length > 0
-        ? [
-            ...pageList.slice(0, numOfTitlesVisible),
-            { pageID: 0, title: "SHOW MORE" },
-          ].map((curr) => {
-            return (
-              <Box
-                onClick={() =>
-                  window.open(
-                    `https://en.wikipedia.org/wiki/${parseForURL(curr.title)}`
-                  )
-                }
-                key={curr.pageID}
-                sx={{
-                  color: "white",
-                  fontWeight: curr.title === "SHOW MORE" ? "bold" : "normal",
-                  padding: 0.5,
-                  "&:hover": {
-                    backgroundColor: "lightblue",
-                    color: "#000000",
-                    cursor: "pointer",
-                  },
-                  "&:active": {
-                    background: "pink",
-                  },
-                }}
-              >
-                {" "}
-                <Typography>{curr.title}</Typography>
-              </Box>
-            );
-          })
-        : null}
-    </Box>
+    <ThemeProvider theme={theme}>
+      <Box
+        display="flex"
+        flexDirection="column"
+        backgroundColor="#000000"
+        alignItems="center"
+        minHeight={"100vh"}
+        padding={5}
+      >
+        <Typography color="white" variant="h1">
+          Wiki
+        </Typography>
+        <TextField
+          variant="filled"
+          label="Search"
+          sx={{ backgroundColor: "white", marginTop: 10 }}
+          value={textInput}
+          onChange={(e) => setTextInput(e.target.value)}
+        />
+        <Button
+          variant="contained"
+          onClick={() => {
+            window.open("https://en.wikipedia.org/wiki/Special:Random");
+          }}
+        >
+          RANDOM ARTICLE
+        </Button>
+        <Button variant="contained" onClick={searchTitle}>
+          GO
+        </Button>
+        <DisplayResults />
+      </Box>
+    </ThemeProvider>
   );
 }
 
